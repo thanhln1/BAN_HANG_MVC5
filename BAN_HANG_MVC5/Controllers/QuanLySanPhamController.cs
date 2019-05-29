20a4c7cfab6e7e88b0740fbb524a2d7f6631c355
thanh <kebinguyenruak52@gmail.com>
2019-05-29 14:24:17 +0700

    add - quan ly don hang

---
+++ BAN_HANG_MVC5/Controllers/QuanLySanPhamController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BAN_HANG_MVC5.Models;
+
+namespace BAN_HANG_MVC5.Controllers
+{
+    public class QuanLySanPhamController : Controller
+    {
+        BanQuanAoEntities2 db = new BanQuanAoEntities2();
+        // GET: QuanLySanPham
+        public ActionResult Index()
+        {
+            return View(db.SanPhams.Where(n => n.DaXoa == false).OrderByDescending(n => n.IdSanPham));
+        }
+    }
+}
---
+++ BAN_HANG_MVC5/Models/ItemGioHang.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BAN_HANG_MVC5.Models
+{
+    public class ItemGioHang
+    {
+        public int IdSanPham { get; set; }
+        public string TenSanPham { get; set; }
+        public int SoLuong { get; set; }
+        public decimal DonGia { get; set; }
+        public decimal TongTien { get; set; }
+        public string HinhAnh { get; set; }
+
+        public ItemGioHang(int idSanPham)
+        {
+            using (BanQuanAoEntities2 db = new BanQuanAoEntities2())
+            {
+                this.IdSanPham = idSanPham;
+                SanPham sanpham = db.SanPhams.Single(n => n.IdSanPham == idSanPham);
+                this.TenSanPham = sanpham.TenSanPham;
+                this.HinhAnh = sanpham.HinhAnh;
+                this.DonGia = sanpham.DonGia.Value;
+                this.SoLuong = 1;
+                this.TongTien = DonGia * SoLuong;
+            }
+        }
+
+        public ItemGioHang(int IdSanPham, int SL)
+        {
+            using (BanQuanAoEntities2 db = new BanQuanAoEntities2())
+            {
+                this.IdSanPham = IdSanPham;
+                SanPham sanpham = db.SanPhams.Single(n => n.IdSanPham == IdSanPham);
+                this.TenSanPham = sanpham.TenSanPham;
+                this.HinhAnh = sanpham.HinhAnh;
+                this.DonGia = sanpham.DonGia.Value;
+                this.SoLuong = SL;
+                this.TongTien = DonGia * SoLuong;
+            }
+        }
+
+    }
+}
